1b9530c6054ef9b4235add4b0bc09fed9869cbcd
Alvin Dennis <alvindennis80@gmail.com>
2025-08-05 23:46:29 +0530

    feat: added auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,31 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+import { timing } from "hono/timing";
 import type { ApiResponse } from "shared/dist";
 
 export const app = new Hono()
+  .use("*", logger())
+  .use("*", cors())
+  .use("*", csrf())
+  .use("*", prettyJSON())
+  .use("*", secureHeaders())
+  .use("*", timing())
 
-.use(cors())
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  })
 
-.get("/", (c) => {
-	return c.text("Hello Hono!");
-})
+  .get("/hello", async (c) => {
+    const data: ApiResponse = {
+      message: "Hello BHVR!",
+      success: true,
+    };
 
-.get("/hello", async (c) => {
-	const data: ApiResponse = {
-		message: "Hello BHVR!",
-		success: true,
-	};
-
-	return c.json(data, { status: 200 });
-});
+    return c.json(data, { status: 200 });
+  })
 
 export default app;
